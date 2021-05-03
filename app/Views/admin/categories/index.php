8a7acc0348023f4f47683f79fbd4b0e1005298be
sahin-webdeveloper <mustafayev.sahin97@gmail.com>
2021-05-03 22:09:03 +0400

    Test

--- app/Views/admin/categories/index.php
+++ app/Views/admin/categories/index.php
@@ -11,7 +11,7 @@
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">Categories Test 3</li>
+                        <li class="breadcrumb-item active">Categories</li>
                     </ol>
                 </div><!-- /.col -->
             </div><!-- /.row -->
@@ -24,7 +24,7 @@
         <div class="card">
             <div class="card-header">
                 <h3 class="card-title"></h3>
-                <a href="/admin/category/add" class="btn btn-primary btn-sm">New category</a>
+                <a href="/admin/category/add" class="btn btn-primary btn-sm">New category Add Test</a>
                 <div class="card-tools">
                     <ul class="pagination pagination-sm float-right">
                         <li class="page-item"><a class="page-link" href="#">&laquo;</a></li>
